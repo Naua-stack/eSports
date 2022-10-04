2ca4807a8483bd2a13d5383b7b1deafe006bb045
Naua-stack <nauafernandesvv@gmail.com>
2022-10-03 21:11:08 -0300

    feat: configure expo notifications

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,7 @@
-import { ActivityIndicator, StatusBar } from "react-native";
+import { StatusBar } from "react-native";
+import { useRef, useEffect } from "react";
 import { Background } from "./src/components/Background";
+import * as Notifications from "expo-notifications";
 import {
   useFonts,
   Inter_400Regular,
@@ -9,6 +11,11 @@ import {
 } from "@expo-google-fonts/inter";
 import { Routes } from "./src/routes";
 import { Loading } from "./src/components/Loading";
+
+import "./src/services/notificationConfig";
+import { getPushNotificationToken } from "./src/services/getPushNotificationToken";
+import { Subscription } from "expo-modules-core";
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
@@ -16,6 +23,39 @@ export default function App() {
     Inter_700Bold,
     Inter_900Black,
   });
+
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
+  }, []);
+
+  useEffect(() => {
+    getNotificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) =>
+        console.log(notification)
+      );
+
+    responseNotificationListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) =>
+        console.log(response)
+      );
+
+    return () => {
+      if (
+        getNotificationListener.current &&
+        responseNotificationListener.current
+      ) {
+        Notifications.removeNotificationSubscription(
+          getNotificationListener.current
+        );
+        Notifications.removeNotificationSubscription(
+          responseNotificationListener.current
+        );
+      }
+    };
+  }, []);
 
   return (
     <Background>
